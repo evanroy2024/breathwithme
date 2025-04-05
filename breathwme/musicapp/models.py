188edbdd9c00edadc41c227ce3d7765f5ee6a8de
erenroy <roypapan75@gmail.com>
2025-04-05 21:19:53 +0530

    aadd
    aadd

--- breathwme/musicapp/models.py
+++ breathwme/musicapp/models.py
@@ -83,12 +83,12 @@ class Playlist(models.Model):
 
 
 
-class GlobalPlaylist(models.Model):
-    name = models.CharField(max_length=255)
-    tracks = models.ManyToManyField(MusicTrack, related_name='global_playlists')
+# class GlobalPlaylist(models.Model):
+#     name = models.CharField(max_length=255)
+#     tracks = models.ManyToManyField(MusicTrack, related_name='global_playlists')
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
 
